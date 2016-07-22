c8515ddaa292936a6c9df3d4bdbf2c5728da8587
José Pedretti Neto <jpedretti@gmail.com>
2016-07-22 14:39:19 -0300

    correção no startup do projeto mvc

--- testes_donet_core/teste_mvc/Startup.cs
+++ testes_donet_core/teste_mvc/Startup.cs
@@ -40,9 +40,6 @@ namespace AspNetCoreTest
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
